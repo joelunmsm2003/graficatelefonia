a78def14349ba2f591d6be0c37cc1223faecf58f
Joel Andy <joel@waqastudios.com>
2017-05-10 00:59:05 -0500

    joel

--- build/js/graph.js
+++ build/js/graph.js
@@ -1,250 +1,334 @@
-// Create the chart
 Highcharts.chart('container', {
     chart: {
         type: 'column'
     },
     title: {
-        text: ''
+        text: 'Llamadas vs Anexo'
     },
-   
+    subtitle: {
+        text: 'Source: xiencias.com'
+    },
     xAxis: {
-        type: 'Anexo'
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
     },
     yAxis: {
+        min: 0,
         title: {
-            text: 'Cantidad total de llamadas'
+            text: 'Cantidad de llamadas'
         }
-
     },
-    legend: {
-        enabled: false
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
     },
     plotOptions: {
-        series: {
-            borderWidth: 0,
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: '101',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+    }, {
+        name: '302',
+        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }, {
+        name: '525',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+    }, {
+        name: '142',
+        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+    }]
+});
+
+Highcharts.chart('grafico2', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Duracion vs Anexo'
+    },
+    subtitle: {
+        text: 'Source: xiencias.com'
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Duracion (seg)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} seg</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: '101',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+    }, {
+        name: '302',
+        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }, {
+        name: '525',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+    }, {
+        name: '142',
+        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+    }]
+});
+
+
+Highcharts.chart('grafico3', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Llamadas vs Anexo'
+    },
+    xAxis: {
+        categories: [
+            '204',
+            '107',
+            '236'
+        ]
+    },
+    yAxis: [{
+        min: 0,
+        title: {
+            text: 'Cantidad'
+        }
+    }, {
+        title: {
+            text: 'Cantidad'
+        },
+        opposite: true
+    }],
+    legend: {
+        shadow: false
+    },
+    tooltip: {
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            grouping: false,
+            shadow: false,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Entrantes',
+        color: 'rgba(165,170,217,1)',
+        data: [150, 73, 20],
+        pointPadding: 0.3,
+        pointPlacement: -0.2
+    }, {
+        name: 'Salientes',
+        color: 'rgba(126,86,134,.9)',
+        data: [140, 90, 40],
+        pointPadding: 0.4,
+        pointPlacement: -0.2
+    }]
+});
+
+Highcharts.chart('grafico4', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Duracion vs Anexo'
+    },
+    xAxis: {
+        categories: [
+            '204',
+            '107',
+            '236'
+        ]
+    },
+    yAxis: [{
+        min: 0,
+        title: {
+            text: 'Duracion'
+        }
+    }, {
+        title: {
+            text: 'Duracion'
+        },
+        opposite: true
+    }],
+    legend: {
+        shadow: false
+    },
+    tooltip: {
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            grouping: false,
+            shadow: false,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Entrantes',
+        color: 'rgba(165,170,217,1)',
+        data: [150, 73, 20],
+        pointPadding: 0.3,
+        pointPlacement: -0.2
+    }, {
+        name: 'Salientes',
+        color: 'rgba(126,86,134,.9)',
+        data: [140, 90, 40],
+        pointPadding: 0.4,
+        pointPlacement: -0.2
+    }]
+});
+
+
+Highcharts.chart('grafico5', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Entrantes'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
             dataLabels: {
                 enabled: true,
-                format: '{point.y:.1f}%'
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
             }
         }
     },
-
-    tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-    },
-
     series: [{
-        name: 'Brands',
+        name: 'Estados',
         colorByPoint: true,
         data: [{
-            name: '100',
-            y: 56.33,
-            drilldown: 'Microsoft Internet Explorer'
-        }, {
-            name: '101',
+            name: 'Contesto',
             y: 24.03,
-            drilldown: '101'
+            sliced: true,
+            selected: true
         }, {
-            name: '102',
-            y: 10.38,
-            drilldown: '102'
+            name: 'No Contesto',
+            y: 10.38
         }, {
-            name: '103',
-            y: 4.77,
-            drilldown: '103'
+            name: 'Cancelado',
+            y: 4.77
         }, {
-            name: '104',
-            y: 0.91,
-            drilldown: '104'
-        }, {
-            name: 'Proprietary or Undetectable',
-            y: 0.2,
-            drilldown: null
+            name: 'Ocupado',
+            y: 0.91
         }]
-    }],
-    drilldown: {
-        series: [{
-            name: '101',
-            id: '101',
-            data: [
-                [
-                    'v11.0',
-                    24.13
-                ],
-                [
-                    'v8.0',
-                    17.2
-                ],
-                [
-                    'v9.0',
-                    8.11
-                ],
-                [
-                    'v10.0',
-                    5.33
-                ],
-                [
-                    'v6.0',
-                    1.06
-                ],
-                [
-                    'v7.0',
-                    0.5
-                ]
-            ]
+    }]
+});
+
+
+Highcharts.chart('grafico6', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Salientes'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Estados',
+        colorByPoint: true,
+        data: [{
+            name: 'Contesto',
+            y: 24.03,
+            sliced: true,
+            selected: true
         }, {
-            name: 'Chrome',
-            id: 'Chrome',
-            data: [
-                [
-                    'v40.0',
-                    5
-                ],
-                [
-                    'v41.0',
-                    4.32
-                ],
-                [
-                    'v42.0',
-                    3.68
-                ],
-                [
-                    'v39.0',
-                    2.96
-                ],
-                [
-                    'v36.0',
-                    2.53
-                ],
-                [
-                    'v43.0',
-                    1.45
-                ],
-                [
-                    'v31.0',
-                    1.24
-                ],
-                [
-                    'v35.0',
-                    0.85
-                ],
-                [
-                    'v38.0',
-                    0.6
-                ],
-                [
-                    'v32.0',
-                    0.55
-                ],
-                [
-                    'v37.0',
-                    0.38
-                ],
-                [
-                    'v33.0',
-                    0.19
-                ],
-                [
-                    'v34.0',
-                    0.14
-                ],
-                [
-                    'v30.0',
-                    0.14
-                ]
-            ]
+            name: 'No Contesto',
+            y: 10.38
         }, {
-            name: 'Firefox',
-            id: 'Firefox',
-            data: [
-                [
-                    'v35',
-                    2.76
-                ],
-                [
-                    'v36',
-                    2.32
-                ],
-                [
-                    'v37',
-                    2.31
-                ],
-                [
-                    'v34',
-                    1.27
-                ],
-                [
-                    'v38',
-                    1.02
-                ],
-                [
-                    'v31',
-                    0.33
-                ],
-                [
-                    'v33',
-                    0.22
-                ],
-                [
-                    'v32',
-                    0.15
-                ]
-            ]
+            name: 'Cancelado',
+            y: 4.77
         }, {
-            name: 'Safari',
-            id: 'Safari',
-            data: [
-                [
-                    'v8.0',
-                    2.56
-                ],
-                [
-                    'v7.1',
-                    0.77
-                ],
-                [
-                    'v5.1',
-                    0.42
-                ],
-                [
-                    'v5.0',
-                    0.3
-                ],
-                [
-                    'v6.1',
-                    0.29
-                ],
-                [
-                    'v7.0',
-                    0.26
-                ],
-                [
-                    'v6.2',
-                    0.17
-                ]
-            ]
-        }, {
-            name: 'Opera',
-            id: 'Opera',
-            data: [
-                [
-                    'v12.x',
-                    0.34
-                ],
-                [
-                    'v28',
-                    0.24
-                ],
-                [
-                    'v27',
-                    0.17
-                ],
-                [
-                    'v29',
-                    0.16
-                ]
-            ]
+            name: 'Ocupado',
+            y: 0.91
         }]
-    }
+    }]
 });
